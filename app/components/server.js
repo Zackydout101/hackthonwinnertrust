065922d7bbc3de7f5e427366c0445c711b3e6dcc
Kristerley <37936327+Kristerley@users.noreply.github.com>
2024-03-02 19:24:41 -0500

    Update server.js

--- app/components/server.js
+++ app/components/server.js
@@ -1,20 +1,45 @@
 // import Script from 'next/script'
+"use client"
 import Link from 'next/link'
+import React, { useEffect, useState } from 'react';
  
 export default function ClassServer({serverInfo}) {
     let index =0;
+    const [messages, setMessages] = useState([]);
 
-    // function handleServerClick(){
-    //     
-    // }
-    function increment(){
-        index = index+1
-    }
-    
-    // let server = serverList[index]
+  useEffect(() => {
+    const socket = new WebSocket('ws:'.concat(serverInfo.address)); // Replace with your WebSocket server URL
+
+    socket.onopen = () => {
+      console.log('WebSocket connection established');
+    };
+
+    socket.onmessage = (event) => {
+      const newMessage = event.data;
+      setMessages((prevMessages) => [...prevMessages, newMessage]);
+    };
+
+    socket.onclose = () => {
+      console.log('WebSocket connection closed');
+    };
+
+    return () => {
+      socket.close();
+    };
+      }, []);    
+
     return (
     <>
         <Link href={serverInfo.route}><h4 class="layer1">{serverInfo.name}</h4></Link>
+    <div>
+      <h2>WebSocket Messages:</h2>
+      <ul>
+        {messages.map((message, index) => (
+          <li key={index}>{message}</li>
+        ))}
+      </ul>
+    </div>
+  );
     </>
     )
 }
